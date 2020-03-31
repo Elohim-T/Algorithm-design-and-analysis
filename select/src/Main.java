83b90457bdac7415b001d91fa18af86a1e0a8130
2018212212198 <1252796750@qq.com>
2020-03-31 20:58:19 +0800

    修改错误

--- select/src/Main.java
+++ select/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int A[] = {0, 1, 2, 3, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-        System.out.println(select(A, 0, A.length - 1, 5));
+        System.out.println(select(A, 0, A.length - 1, 1));
     }
 
     static int r = 5;
@@ -15,13 +15,11 @@ public class Main {
         int[] M = new int[group];
 
         for (int i = 0; i < group; i++) {
-            Arrays.sort(A, low + r * i, low + r * i + (num > r * i +r? r : num - i * r));
-            M[i] = A[low + r * i + (num > r * i +r? r : num - i * r) / 2];
+            Arrays.sort(A, low + r * i, low + r * i + (num > r * i + r ? r : num - i * r));
+            M[i] = A[low + r * i + (num > r * i + r ? r : num - i * r) / 2];
         }
-        //对M[]进行排序，并选M[]的中位数m
-        Arrays.sort(M,0,group-1);
-        int mid = M[M.length/2];
-
+        //选M[]的中位数m
+        int mid = M.length <= 2 ? M[0] : select(M, 0, M.length - 1, M.length / 2);
         int[] S1 = new int[num];//小于mid的数
         int[] S2 = new int[num];//大于mid的数
         int numS1, numS2, numS3;
@@ -39,9 +37,9 @@ public class Main {
         if (numS1 >= k) {
             //小于mid的数的数量对大于等于k
             return select(S1, 0, numS1 - 1, k);
-        }else if (numS1 + numS3 >= k) {
+        } else if (numS1 + numS3 >= k) {
             return mid;
-        }else{
+        } else {
             //小于等于mid的数的数量小于k
             return select(S2, 0, numS2 - 1, k - numS1 - numS3);
         }
